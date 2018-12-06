02b0a640d71e442f6f02e8ad1fbd2127aca04b20
Mark Murray <mark@murray.xyz>
2018-12-06 06:53:01 +0000

    Day 6. Part 1.

--- 2018/06/1.hs
+++ 2018/06/1.hs
@@ -1,4 +1,5 @@
 import qualified Data.List.Split as Split
+import qualified Data.Set as Set
 import Data.List
 
 distance :: (Int, Int) -> (Int, Int) -> Int
@@ -12,8 +13,27 @@ parseLine line =
     (read x, read y)
 
 findClosestTo :: [(Int, Int)] -> (Int, Int) -> (Int, Int)
-findClosestTo points (tx, ty) =
-  minimumBy (\(x1, y1) (x2, y2) -> compare (distance (x1, y1) (tx, ty)) (distance (x2, y2) (tx, ty))) points
+findClosestTo points target =
+  let
+    distances = sort $ map (distance target) points
+    equidistant = (head distances) == (head . tail $ distances)
+  in
+    if equidistant
+      then (0, 0)
+      else minimumBy (\(x1, y1) (x2, y2) -> compare (distance (x1, y1) target) (distance (x2, y2) target)) points
+
+perimeterPoints :: (Int, Int) -> (Int, Int) -> [(Int, Int)]
+perimeterPoints (x1, y1) (x2, y2) = nub $
+  [(x, y1) | x <- [x1..x2]] ++
+  [(x2, y) | y <- [y1..y2]] ++
+  [(x, y2) | x <- [x1..x2]] ++
+  [(x1, y) | y <- [y1..y2]]
+
+areaPoints :: (Int, Int) -> (Int, Int) -> [(Int, Int)]
+areaPoints (x1, y1) (x2, y2) = [(x, y) | x <- [x1..x2], y <- [y1..y2]]
+
+countPoints :: [(Int, Int)] -> (Int, Int) -> Int
+countPoints points target = length $ filter (== target) points
 
 main = do
   contents <- getContents
@@ -21,5 +41,13 @@ main = do
     allLines = lines contents
     points = map parseLine allLines
     p = findClosestTo points (1,2)
-  print $ points
-  print p
+    bound1 = (0, 0)
+    bound2 = (500, 500)
+    perimeter = perimeterPoints bound1 bound2
+    area = areaPoints bound1 bound2
+    infiniteRegions = nub $ map (findClosestTo points) perimeter
+    voronoiPoints = map (findClosestTo points) area
+    finiteVoronoiRegions = Set.toList $ Set.difference (Set.fromList voronoiPoints) (Set.fromList infiniteRegions)
+    finiteVoronoiAreas = map (countPoints voronoiPoints) finiteVoronoiRegions
+    largestArea = last . sort $ finiteVoronoiAreas
+  print largestArea
