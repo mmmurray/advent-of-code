f718fe185126f0eb9be86af34602a7dc5fec41f3
Mark Murray <mark@murray.xyz>
2018-12-12 08:12:26 +0000

    Day 12. Part 2.

--- 2018/12/1+2.hs
+++ 2018/12/1+2.hs
@@ -1,7 +1,5 @@
-import Debug.Trace
 import Text.Regex.PCRE
 import qualified Data.List as List
-import qualified Data.Set as Set
 import qualified Data.Map.Strict as Map
 
 type Rules = Map.Map (Bool, Bool, Bool, Bool, Bool) Bool
@@ -29,42 +27,43 @@ parseInput input =
     (initialState, rules)
 
 
-hasPot :: Pots -> Pot -> Bool
-hasPot (p:ps) pot
+hasPlant :: Pots -> Pot -> Bool
+hasPlant (p:ps) pot
   | p == pot = True
   | p > pot = False
   | length ps == 0 = False
-  | otherwise = hasPot ps pot
+  | otherwise = hasPlant ps pot
 
 
 nextPotState :: Rules -> Pots -> Pot -> Bool
 nextPotState rules pots pot =
   let
-    l2 = hasPot pots (pot - 2)
-    l1 = hasPot pots (pot - 1)
-    c = hasPot pots pot
-    r1 = hasPot pots (pot + 1)
-    r2 = hasPot pots (pot + 2)
+    l2 = hasPlant pots (pot - 2)
+    l1 = hasPlant pots (pot - 1)
+    c = hasPlant pots pot
+    r1 = hasPlant pots (pot + 1)
+    r2 = hasPlant pots (pot + 2)
   in
     case Map.lookup (l2, l1, c, r1, r2) rules of
       Just state -> state
       Nothing -> False
 
 
-evolve :: Rules -> Pots -> Int -> Pots
-evolve rules pots generations =
+evolve :: Rules -> Pots -> Pots
+evolve rules pots =
   let
     min = head pots - 2
     max = last pots + 2
-    newPots = List.filter (nextPotState rules pots) [min..max]
-  in if generations == 1
-    then newPots
-    else evolve rules newPots (generations - 1)
+  in List.filter (nextPotState rules pots) [min..max]
+
+
+evolveGenerations :: Rules -> Pots -> Int -> Pots
+evolveGenerations rules pots generations = List.foldl' (\acc g -> evolve rules acc) pots [1..generations]
 
 
 main = do
   contents <- getContents
   let
     (initialState, rules) = parseInput contents
-  print $ "Part 1: " ++ (show $ sum (evolve rules initialState 20))
-  print $ "Part 2: " ++ (show $ (evolve rules initialState 50000000000))
+  print $ "Part 1: " ++ (show $ sum (evolveGenerations rules initialState 20))
+  print $ "Part 2: " ++ (show $ sum (evolveGenerations rules initialState 10000))
