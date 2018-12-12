c723ab1b929867f24537674a5e1855521ada0f15
Mark Murray <mark@murray.xyz>
2018-12-12 06:41:55 +0000

    Day 12. Part 1.

--- 2018/12/1.hs
+++ 2018/12/1.hs
@@ -1,27 +1,58 @@
+import Text.Regex.PCRE
 import qualified Data.Set as Set
 import qualified Data.Map.Strict as Map
 
-type Rules = Map.Map (Bool, Bool, Bool, Bool, Bool, Bool) Bool
+type Rules = Map.Map (Bool, Bool, Bool, Bool, Bool) Bool
 type Pot = Int
 type Pots = Set.Set Pot
+
+parseRule :: String -> ((Bool, Bool, Bool, Bool, Bool), Bool)
+parseRule rule =
+  let
+    matches = rule =~ "^(\\.|#)(\\.|#)(\\.|#)(\\.|#)(\\.|#) => (\\.|#)$" :: [[String]]
+    [l2, l1, a, r1, r2, b] = map (== "#") $ tail . head $ matches
+  in
+    ((l2, l1, a, r1, r2), b)
+
 
 parseInput :: String -> (Pots, Rules)
 parseInput input =
   let
     (initialStateLine:ruleLines) = lines input
-
-    rules = Map.fromList []
-    initialState = Set.fromList []
+    pots = zip [0..] (drop (length "initial state: ") initialStateLine)
+    potsWithPlant = filter (\(i, c) -> c == '#') pots
+    initialState = Set.fromList $ map fst potsWithPlant
+    rules = Map.fromList (map parseRule (tail ruleLines))
   in
     (initialState, rules)
 
-evolve :: Rules -> Pots -> Pots
-evolve rules pots = pots
+
+nextPotState :: Rules -> Pots -> Pot -> Bool
+nextPotState rules pots pot =
+  let
+    l2 = (pot - 2) `Set.member` pots
+    l1 = (pot - 1) `Set.member` pots
+    c = pot `Set.member` pots
+    r1 = (pot + 1) `Set.member` pots
+    r2 = (pot + 2) `Set.member` pots
+  in
+    case Map.lookup (l2, l1, c, r1, r2) rules of
+      Just state -> state
+      Nothing -> False
+
+
+evolve :: Rules -> Pots -> Int -> Pots
+evolve rules pots generations =
+  let
+    min = Set.findMin pots - 2
+    max = Set.findMax pots + 2
+    newPots = Set.fromList $ filter (nextPotState rules pots) [min..max]
+  in if generations == 1 then newPots else evolve rules newPots (generations - 1)
+
 
 main = do
   contents <- getContents
   let
     (initialState, rules) = parseInput contents
-  print $ initialState
-  print $ rules
-  
+    finalState = evolve rules initialState 20
+  print $ sum finalState
