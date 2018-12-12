b198379ea855c020cd06eed37d6005d5cb9900df
Mark Murray <mark@murray.xyz>
2018-12-12 07:39:00 +0000

    Day 12. Part 2.

--- 2018/12/1+2.hs
+++ 2018/12/1+2.hs
@@ -6,7 +6,7 @@ import qualified Data.Map.Strict as Map
 
 type Rules = Map.Map (Bool, Bool, Bool, Bool, Bool) Bool
 type Pot = Int
-type Pots = Set.Set Pot
+type Pots = [Pot]
 
 parseRule :: String -> ((Bool, Bool, Bool, Bool, Bool), Bool)
 parseRule rule =
@@ -23,20 +23,28 @@ parseInput input =
     (initialStateLine:ruleLines) = lines input
     pots = zip [0..] (drop (length "initial state: ") initialStateLine)
     potsWithPlant = filter (\(i, c) -> c == '#') pots
-    initialState = Set.fromList $ map fst potsWithPlant
+    initialState = map fst potsWithPlant
     rules = Map.fromList (map parseRule (tail ruleLines))
   in
     (initialState, rules)
 
 
+hasPot :: Pots -> Pot -> Bool
+hasPot (p:ps) pot
+  | p == pot = True
+  | p > pot = False
+  | length ps == 0 = False
+  | otherwise = hasPot ps pot
+
+
 nextPotState :: Rules -> Pots -> Pot -> Bool
 nextPotState rules pots pot =
   let
-    l2 = (pot - 2) `Set.member` pots
-    l1 = (pot - 1) `Set.member` pots
-    c = pot `Set.member` pots
-    r1 = (pot + 1) `Set.member` pots
-    r2 = (pot + 2) `Set.member` pots
+    l2 = hasPot pots (pot - 2)
+    l1 = hasPot pots (pot - 1)
+    c = hasPot pots pot
+    r1 = hasPot pots (pot + 1)
+    r2 = hasPot pots (pot + 2)
   in
     case Map.lookup (l2, l1, c, r1, r2) rules of
       Just state -> state
@@ -46,13 +54,9 @@ nextPotState rules pots pot =
 evolve :: Rules -> Pots -> Int -> Pots
 evolve rules pots generations =
   let
-    min = Set.findMin pots - 2
-    max = Set.findMax pots + 2
-    newPots = List.foldr f pots [min..max]
-      where
-        f pot acc = if nextPotState rules pots pot
-          then Set.insert pot acc
-          else Set.delete pot acc
+    min = head pots - 2
+    max = last pots + 2
+    newPots = List.filter (nextPotState rules pots) [min..max]
   in if generations == 1
     then newPots
     else evolve rules newPots (generations - 1)
@@ -63,4 +67,4 @@ main = do
   let
     (initialState, rules) = parseInput contents
   print $ "Part 1: " ++ (show $ sum (evolve rules initialState 20))
-  print $ "Part 2: " ++ (show $ sum (evolve rules initialState 50000000))
+  print $ "Part 2: " ++ (show $ (evolve rules initialState 50000000000))
