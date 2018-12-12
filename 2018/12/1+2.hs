39f029af67b257f8209c4d0ee8f6b6aa3783b4a2
Mark Murray <mark@murray.xyz>
2018-12-12 07:18:16 +0000

    Day 12. Part 2 using sets wip

--- 2018/12/1+2.hs
+++ 2018/12/1+2.hs
@@ -1,4 +1,6 @@
+import Debug.Trace
 import Text.Regex.PCRE
+import qualified Data.List as List
 import qualified Data.Set as Set
 import qualified Data.Map.Strict as Map
 
@@ -46,13 +48,19 @@ evolve rules pots generations =
   let
     min = Set.findMin pots - 2
     max = Set.findMax pots + 2
-    newPots = Set.fromList $ filter (nextPotState rules pots) [min..max]
-  in if generations == 1 then newPots else evolve rules newPots (generations - 1)
+    newPots = List.foldr f pots [min..max]
+      where
+        f pot acc = if nextPotState rules pots pot
+          then Set.insert pot acc
+          else Set.delete pot acc
+  in if generations == 1
+    then newPots
+    else evolve rules newPots (generations - 1)
 
 
 main = do
   contents <- getContents
   let
     (initialState, rules) = parseInput contents
-    finalState = evolve rules initialState 20
-  print $ sum finalState
+  print $ "Part 1: " ++ (show $ sum (evolve rules initialState 20))
+  print $ "Part 2: " ++ (show $ sum (evolve rules initialState 50000000))
