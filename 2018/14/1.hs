2e4482bbe0a50a1b3a0027a7f1f2ac43e2019c69
Mark Murray <mark@murray.xyz>
2018-12-14 16:39:22 +0000

    refactor

--- 2018/14/1.hs
+++ 2018/14/1.hs
@@ -1,27 +1,35 @@
+type Elf = (Int, Int)
+
+
 parseDigit :: Char -> Int
 parseDigit c = read [c]
 
 
-newCurrent :: [Int] -> Int -> Int
-newCurrent scoreboard current =
+newCurrent :: [Int] -> Int -> Elf -> Elf
+newCurrent scoreboard offset (index, value) =
   let
-    value = (!!) scoreboard current
+    newIndex = ((index + offset) - value - 1) `mod` (length scoreboard)
+    newValue = (!!) scoreboard newIndex
   in
-    (current + value + 1) `mod` (length scoreboard)
+    (newIndex, newValue)
 
 
-createRecipes :: ([Int], [Int]) -> ([Int], [Int])
+createRecipes :: ([Int], [Elf]) -> ([Int], [Elf])
 createRecipes (scoreboard, elves) =
   let
-    current = map ((!!) scoreboard) elves
-    newRecipes = map parseDigit $ show $ sum current
-    newScoreboard = scoreboard ++ newRecipes
-    newElves = map (newCurrent newScoreboard) elves
+    current = map snd elves
+    result = sum current
+
+    (newScoreboard, offset) = if result >= 10
+      then ((result - 10) : 1 : scoreboard, 2)
+      else (result : scoreboard, 1)
+
+    newElves = map (newCurrent newScoreboard offset) elves
   in
     (newScoreboard, newElves)
 
 
-recipes :: ([Int], [Int]) -> Int -> [Int]
+recipes :: ([Int], [Elf]) -> Int -> [Int]
 recipes (scoreboard, elves) n
   | length scoreboard < n = recipes (createRecipes (scoreboard, elves)) n
   | otherwise = scoreboard
@@ -31,6 +39,6 @@ main = do
   contents <- getContents
   let
     count = 47801
-    scoreboard = recipes ([3, 7], [0, 1]) (count + 10)
-    result = take 10 $ drop count scoreboard
-  print result
+    scoreboard = recipes ([7, 3], [(1, 3), (0, 7)]) (count + 10)
+    result = take 10 $ drop count (reverse scoreboard)
+  print $ "Part 1: " ++ map (head . show) result
