9683806543c3ca8be63f2ce6d9a90dacdf5d8f3a
Mark Murray <mark@murray.xyz>
2018-12-15 16:56:38 +0000

    Day 15. Part 1.

--- 2018/15/1.hs
+++ 2018/15/1.hs
@@ -4,7 +4,7 @@ import qualified Data.Map as Map
 import qualified Data.Set as Set
 
 type Point = (Int, Int)
-type Player = (Point, Int, Bool) -- position, HP, is goblin
+type Player = (Int, Point, Int, Bool) -- id, position, HP, is goblin
 type Walls = Set.Set Point
 
 initialHP = 200
@@ -18,7 +18,6 @@ adjcentPoints (x, y) = [(x, y - 1), (x + 1, y), (x, y + 1), (x - 1, y)]
 
 dijkstra :: Set.Set Point -> Map.Map Point Int -> [(Point, Int)] -> Map.Map Point Int
 dijkstra spaces found queue
-  -- | trace (show found) False = undefined
   | length queue == 0 = found
   | otherwise =
       let
@@ -42,18 +41,18 @@ loadMap input = foldl f ([], []) (parseInput input)
       let
         (newWalls, newPlayers) = case char of
           '#' -> ([position], [])
-          'E' -> ([], [(position, initialHP, False)])
-          'G' -> ([], [(position, initialHP, True)])
+          'E' -> ([], [(0, position, initialHP, False)])
+          'G' -> ([], [(0, position, initialHP, True)])
       in
         (walls ++ newWalls, players ++ newPlayers)
 
 
 sortPlayers :: [Player] -> [Player]
-sortPlayers = List.sortBy (\(p1, _, _) (p2, _, _) -> readingOrder p1 p2)
+sortPlayers = List.sortBy (\(_, p1, _, _) (_, p2, _, _) -> readingOrder p1 p2)
 
 
 sortWeakest :: [Player] -> [Player]
-sortWeakest = List.sortBy (\(_, hp1, _) (_, hp2, _) -> compare hp1 hp2)
+sortWeakest = List.sortBy (\(_, _, hp1, _) (_, _, hp2, _) -> compare hp1 hp2)
 
 
 readingOrder :: Point -> Point -> Ordering
@@ -73,7 +72,7 @@ isAdjacent (x1, y1) (x2, y2)
 
 
 enemiesInRange :: [Player] -> Player -> [Player]
-enemiesInRange players (p1, _, t1) = filter (\(p2, _, t2) -> t1 /= t2 && isAdjacent p1 p2) players
+enemiesInRange players (_, p1, _, t1) = filter (\(_, p2, _, t2) -> t1 /= t2 && isAdjacent p1 p2) players
 
 
 attackEnemyInRange :: [Player] -> Player -> (Bool, [Player])
@@ -81,28 +80,27 @@ attackEnemyInRange players player =
   let
     targets = sortWeakest $ sortPlayers $ enemiesInRange players player
     attackerPosition = if length targets > 0
-      then (let (p2, _, _) = head targets in p2)
+      then (let (_, p2, _, _) = head targets in p2)
       else noPoint
 
-    damagedPlayers = map (\(p2, hp, t2) -> if attackerPosition == p2 then (p2, hp - attackDamage, t2) else (p2, hp, t2)) players
+    damagedPlayers = map (\(id2, p2, hp, t2) -> if attackerPosition == p2 then (id2, p2, hp - attackDamage, t2) else (id2, p2, hp, t2)) players
 
-    newPlayers = filter (\(_, hp, _) -> hp > 0) damagedPlayers
+    newPlayers = filter (\(_, _, hp, _) -> hp > 0) damagedPlayers
   in
     (attackerPosition /= noPoint, newPlayers)
 
 
 movePlayer :: Walls -> [Player] -> Player -> (Player, [Player])
-movePlayer walls players (p1, hp1, t1) =
+movePlayer walls players (id1, p1, hp1, t1) =
   let
     (xMax, yMax) = Set.findMax walls
-    playerPoints = Set.fromList $ map (\(p, _, _) -> p) players
+    playerPoints = Set.fromList $ map (\(_, p, _, _) -> p) players
     allPoints = Set.fromList [(x, y) | x <- [0..xMax], y <- [0..yMax]]
     spaces = allPoints Set.\\ walls Set.\\ playerPoints
-
-    enemies = filter (\(_, _, t2) -> t1 /= t2) players
+    enemies = filter (\(_, _, _, t2) -> t1 /= t2) players
     adjcent = adjcentPoints p1
 
-    f (p2, _, _) =
+    f (_, p2, _, _) =
       let
         distances = dijkstra spaces (Map.fromList [(p2, 0)]) [(p2, 0)]
         possible = filter (\p -> Map.member p distances && Set.member p spaces) adjcent
@@ -113,17 +111,14 @@ movePlayer walls players (p1, hp1, t1) =
     readingOrderCandidates = List.sortBy (\(p1, d1) (p2, d2) -> readingOrder p1 p2) candidates
     bestCandidates = List.sortBy (\(p1, d1) (p2, d2) -> compare d1 d2) readingOrderCandidates
     moveTo = if length bestCandidates > 0 then (fst . head) bestCandidates else noPoint
-    newPlayer = if moveTo == noPoint then (p1, hp1, t1) else (moveTo, hp1, t1)
-    newPlayers = map (\(p2, hp, t) -> if p2 == p1 then newPlayer else (p2, hp, t)) players
+    newPlayer = if moveTo == noPoint then (id1, p1, hp1, t1) else (id1, moveTo, hp1, t1)
+    newPlayers = map (\(id2, p2, hp, t) -> if p2 == p1 then newPlayer else (id2, p2, hp, t)) players
   in
     (newPlayer, newPlayers)
 
 
 takeTurn :: Walls -> [Player] -> Player -> [Player]
-takeTurn walls players player
-  -- | trace ("Player turn: " ++ (show player)) False = undefined
-  -- | trace ("Player turn: " ++ (show player) ++ "\n" ++ (printPoints $ renderState walls players)) False = undefined
-  | otherwise =
+takeTurn walls players player =
   let
     shouldMove = length (enemiesInRange players player) == 0
     (movedPlayer, movedPlayers) = if shouldMove then movePlayer walls players player else (player, players)
@@ -132,40 +127,42 @@ takeTurn walls players player
     attackedPlayers
 
 
+playerInList :: [Player] -> Player -> Bool
+playerInList players (id1, _, _, _) = length (filter (\(id2, _, _, _) -> id1 == id2) players) == 1
+
+
 takeTurns :: Walls -> [Player] -> [Player]
 takeTurns walls players =
   let
     orderedPlayers = sortPlayers players
   in
-    List.foldl' (\acc player -> takeTurn walls acc player) orderedPlayers orderedPlayers
-    -- List.foldl' (\acc player -> if List.elem player acc then takeTurn walls acc player else acc) orderedPlayers orderedPlayers
+    List.foldl' (\acc player -> if playerInList acc player then takeTurn walls acc player else acc) orderedPlayers orderedPlayers
 
 
 battleComplete :: [Player] -> Bool
 battleComplete players =
   let
-    elves = (map (\(p, _, _) -> p) (filter (\(_, _, t) -> not t) players))
-    goblins = (map (\(p, _, _) -> p) (filter (\(_, _, t) -> t) players))
+    elves = (map (\(_, p, _, _) -> p) (filter (\(_, _, _, t) -> not t) players))
+    goblins = (map (\(_, p, _, _) -> p) (filter (\(_, _, _, t) -> t) players))
   in
     (length elves == 0) || (length goblins == 0)
 
 
 totalHP :: [Player] -> Int
-totalHP players = sum (map (\(_, hp, _) -> hp) players)
+totalHP players = sum (map (\(_, _, hp, _) -> hp) players)
 
 
 battle :: Walls -> [Player] -> Int -> Int
 battle walls players turn
-  | trace ((show turn) ++ (":\n") ++ (printPlayers players) ++ ("\n") ++ (printPoints $ renderState walls players)) False = undefined
-  -- | turn == 30 = 0
-  | battleComplete players = turn * (totalHP players)
+  -- | trace ((show turn) ++ (":\n") ++ (printPlayers players) ++ ("\n") ++ (printPoints $ renderState walls players)) False = undefined
+  | battleComplete players = (turn - 1) * (totalHP players)
   | otherwise = battle walls (takeTurns walls players) (turn + 1)
 
 
 printPlayers :: [Player] -> String
 printPlayers players =
   let
-    parts = map (\(p, hp, t) -> (if t then "G" else "E") ++ "(" ++ (show hp) ++ ")" ++ (show p)) (sortPlayers players)
+    parts = map (\(id, p, hp, t) -> (if t then "G" else "E") ++ (show id) ++ "(" ++ (show hp) ++ ")" ++ (show p)) (sortPlayers players)
   in
     List.intercalate ", " parts
 
@@ -184,8 +181,8 @@ renderStateWithDistance walls players distances =
   let
     (xMax, yMax) = Set.findMax walls
     allPoints = [(x, y) | x <- [0..xMax], y <- [0..yMax]]
-    elves = Set.fromList (map (\(p, _, _) -> p) (filter (\(_, _, t) -> not t) players))
-    goblins = Set.fromList (map (\(p, _, _) -> p) (filter (\(_, _, t) -> t) players))
+    elves = Set.fromList (map (\(_, p, _, _) -> p) (filter (\(_, _, _, t) -> not t) players))
+    goblins = Set.fromList (map (\(_, p, _, _) -> p) (filter (\(_, _, _, t) -> t) players))
   in Map.fromList (map (\p -> (p, f p elves goblins)) allPoints)
       where f p elves goblins
               | Map.member p distances = head $ show $ distances Map.! p
@@ -194,30 +191,25 @@ renderStateWithDistance walls players distances =
               | Set.member p goblins = 'G'
               | otherwise = '.'
 
+
 renderState :: Walls -> [Player] -> Map.Map Point Char
 renderState walls players = renderStateWithDistance walls players (Map.fromList [])
+
+
+playerWithId :: Player -> Int -> Player
+playerWithId (_, p, hp, t) id = (id, p, hp, t)
+
 
 main = do
   contents <- getContents
   let
-    (wallPoints, players) = loadMap contents
+    (wallPoints, playersWithoutIds) = loadMap contents
+    players = map (\(id, player) -> playerWithId player id) (zip [0..] playersWithoutIds)
     (xMax, yMax) = maximum wallPoints
     allPointsList = [(x, y) | x <- [0..xMax], y <- [0..yMax]]
     allPoints = Set.fromList allPointsList
     walls = Set.fromList wallPoints
-    playerPoints = Set.fromList $ map (\(p, _, _) -> p) players
+    playerPoints = Set.fromList $ map (\(_, p, _, _) -> p) players
     spaces = allPoints Set.\\ walls
-    -- distances = dijkstra spaces (Map.fromList [((1, 1), 0)]) [((1, 1), 0)]
-
     result = battle walls players 0
-
-
-  -- print $ players
-  -- print $ players2
-  -- print $ isAdjacent (1, 2) (1, 3)
-  -- print $ takeTurn walls players
-  -- print $ spaces
-  -- print $ distances
-  -- putStr $ printPoints $ renderState walls players
-  -- putStr $ printPoints $ renderState walls players2
   print result
