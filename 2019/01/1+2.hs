9c1c5b0a00100b07ac04ef2323061e1c4a9decea
Mark Murray <mark@murray.xyz>
2019-12-01 13:04:25 +0000

    style: remove newline

--- 2019/01/1+2.hs
+++ 2019/01/1+2.hs
@@ -7,7 +7,6 @@ fuelRequiredRec m = fuelRequired m + fuelRequiredRec (fuelRequired m)
 
 main = do
   contents <- getContents
-  let
-    masses = map read (lines contents)
+  let masses = map read (lines contents)
   print $ "Part 1: " ++ (show $ sum . map fuelRequired $ masses)
   print $ "Part 2: " ++ (show $ sum . map fuelRequiredRec $ masses)
