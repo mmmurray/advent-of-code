86b7fccad2abe102b8b61939569358f8573d1eed
Mark Murray <mark@murray.xyz>
2019-12-08 16:05:29 +0000

    refactor

--- 2019/08/1+2.hs
+++ 2019/08/1+2.hs
@@ -1,5 +1,6 @@
 import Data.List.Split (chunksOf)
-import Data.List (intercalate, sortBy)
+import Data.List (intercalate, minimumBy)
+import Data.Ord (comparing)
 
 parseInput :: String -> [Int]
 parseInput "" = []
@@ -12,19 +13,24 @@ part1 :: [[Int]] -> Int
 part1 layers =
   let
     c0 = countDigit 0
-    layer0 = head $ sortBy (\a b -> compare (c0 a) (c0 b)) layers
+    layer0 = minimumBy (comparing c0) layers
   in (countDigit 1 layer0) * (countDigit 2 layer0)
 
 resolveLayers :: [[Int]] -> [Int]
 resolveLayers [] = []
 resolveLayers [l] = l
-resolveLayers (l1:ls) = zipWith (\p1 p2 -> if p1 == 2 then p2 else p1) l1 (resolveLayers ls)
+resolveLayers (l1:ls) = zipWith overlay l1 (resolveLayers ls)
+  where
+    overlay 2 p = p
+    overlay p _ = p
 
 printImage :: Int -> [Int] -> String
 printImage width pixels = intercalate "\n" $ map printLine (chunksOf width pixels)
   where
+    printPixel 1 = '#'
+    printPixel _ = ' '
     printLine [] = ""
-    printLine (p:ps) = (if p == 1 then '#' else ' ') : (printLine ps)
+    printLine (p:ps) = printPixel p : printLine ps
 
 part2 :: [[Int]] -> [Int]
 part2 layers = resolveLayers layers
